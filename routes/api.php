1a393311b7cfb6844c36f5a6375987e87f9b31e7
Jean-Lionel <nijeanlionel@gmail.com>
2022-05-10 13:56:07 +0200

    first laravel deployment to heroku

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/articles', [ArticleController::class, 'store']);
+Route::get("articles", [ArticleController::class, "index"]);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get("articles", [ArticleController::class, "index"]);
+   
     Route::get('/me',[AuthController::class, 'me']);
+    Route::post('/articles', [ArticleController::class, 'store']);
 
     Route::post('/logout', [AuthController::class, 'logout']);
 });
