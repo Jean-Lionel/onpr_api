4baa429d1aaac667d907b381ee027ca3536bd0cf
Jean-Lionel <nijeanlionel@gmail.com>
2022-05-10 07:04:52 +0200

    remove passport authentifiction

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,38 +7,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-
-/**
-
-    @OA\Info(
-    title="Your super ApplicationAPI",
-    version="1.0.0",
-    )
-*/
-
 class Controller extends BaseController
 {
-     /**
-     * @OA\Info(
-     *      version="1.0.0",
-     *      title="Api covid19 Documentation",
-     *      description="Implementation of Swagger with in Laravel",
-     *      @OA\Contact(
-     *          email="admin@admin.com"
-     *      ),
-     *      @OA\License(
-     *          name="Apache 2.0",
-     *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
-     *      )
-     * )
-     *
-     * @OA\Server(
-     *      url=L5_SWAGGER_CONST_HOST,
-     *      description="Demo API Server"
-     * )
-
-     *
-     *
-     */
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
