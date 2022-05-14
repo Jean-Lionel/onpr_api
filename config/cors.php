cf777f6003e263f38fe375c3c3e9dc4f9fe22083
Jean-Lionel <nijeanlionel@gmail.com>
2022-05-14 17:15:53 +0200

    update htaccess

--- config/cors.php
+++ config/cors.php
@@ -15,13 +15,13 @@ return [
     |
     */
 
-    'paths' => ['*', 'sanctum/csrf-cookie','/api/*'],
+    'paths' => ['/api/*', 'sanctum/csrf-cookie','*'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => ["http://localhost:3000","*"],
+    'allowed_origins_patterns' => ["*"],
 
     'allowed_headers' => ['*'],
 
