ad02a757b3ee538b59ecb41aef7b41058de7671f
Jean-Lionel <nijeanlionel@gmail.com>
2022-05-16 09:52:58 +0200

    created releve des salaires

---
+++ app/Http/Controllers/CotisationAfilierController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CotisationAfilier;
+use App\Http\Requests\StoreCotisationAfilierRequest;
+use App\Http\Requests\UpdateCotisationAfilierRequest;
+
+class CotisationAfilierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCotisationAfilierRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCotisationAfilierRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CotisationAfilier  $cotisationAfilier
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CotisationAfilier $cotisationAfilier)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCotisationAfilierRequest  $request
+     * @param  \App\Models\CotisationAfilier  $cotisationAfilier
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCotisationAfilierRequest $request, CotisationAfilier $cotisationAfilier)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CotisationAfilier  $cotisationAfilier
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CotisationAfilier $cotisationAfilier)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_05_16_074438_create_cotisation_afiliers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCotisationAfiliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cotisation_afiliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('matricule');
+            $table->integer('mois');
+            $table->integer('annee');
+            $table->double('cotisation_employee', 64,2)->default(0);
+            $table->double('salaire_base',64,2)->default(0);
+            $table->double('points',64,2)->default(0);
+            $table->foreignId('user_id');
+            $table->foreignId('institution_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cotisation_afiliers');
+    }
+}
