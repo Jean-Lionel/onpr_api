15d449d7ee52b1a41e8239ec5f5122de8d58560c
Jean-Lionel <nijeanlionel@gmail.com>
2022-05-16 09:42:35 +0200

    add afilier table

---
+++ app/Models/Afilier.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Afilier extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2022_05_16_074122_create_afiliers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAfiliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('afiliers', function (Blueprint $table) {
+            $table->id();
+            $table->string("nom")->nullable();
+            $table->string("prenom")->nullable();
+            $table->string("matricule");
+            $table->string("telephone")->nullable();
+            $table->double("salaire_base", 64,2)->default(0);
+            $table->string("email")->nullable();
+            $table->string("debut_travail")->nullable();
+            $table->foreignId("institution_id")->nullable();
+            $table->foreignId("user_id")->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('afiliers');
+    }
+}
