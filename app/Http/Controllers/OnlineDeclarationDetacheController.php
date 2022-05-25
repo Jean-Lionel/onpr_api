30135f285e34b0d60273ec92645d9d9ce31d314c
Jean-Lionel <nijeanlionel@gmail.com>
2022-05-25 03:54:50 +0200

    add file name

---
+++ app/Http/Controllers/OnlineDeclarationDetacheController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OnlineDeclarationDetache;
+use App\Http\Requests\StoreOnlineDeclarationDetacheRequest;
+use App\Http\Requests\UpdateOnlineDeclarationDetacheRequest;
+
+class OnlineDeclarationDetacheController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreOnlineDeclarationDetacheRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreOnlineDeclarationDetacheRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OnlineDeclarationDetache  $onlineDeclarationDetache
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OnlineDeclarationDetache $onlineDeclarationDetache)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateOnlineDeclarationDetacheRequest  $request
+     * @param  \App\Models\OnlineDeclarationDetache  $onlineDeclarationDetache
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateOnlineDeclarationDetacheRequest $request, OnlineDeclarationDetache $onlineDeclarationDetache)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OnlineDeclarationDetache  $onlineDeclarationDetache
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OnlineDeclarationDetache $onlineDeclarationDetache)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_05_24_173408_create_online_declaration_detaches_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOnlineDeclarationDetachesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('online_declaration_detaches', function (Blueprint $table) {
+            $table->id();
+            $table->string('code_instution');
+            $table->string('nom_instution');
+            $table->string('mois');
+            $table->string('annee');
+            $table->date('date_declaration');
+            $table->longText('description')->nullable();
+            $table->string('file_name_1')->nullable();
+            $table->string('file_uploaded_1');
+            $table->string('file_name_2')->nullable();
+            $table->string('file_uploaded_2')->nullable();
+            $table->string('file_name_3')->nullable();
+            $table->string('file_uploaded_3')->nullable();
+            $table->foreignId('user_id');
+            $table->boolean('is_opened')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('online_declaration_detaches');
+    }
+}
