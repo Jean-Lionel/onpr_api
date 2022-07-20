a0f0b1505041b9a717c6775a719b8f7099cbf8c8
Jean-Lionel <nijeanlionel@gmail.com>
2022-07-20 06:01:00 +0200

    adding any easy way

--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+header('Access-Control-Allow-Origin: *');  
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
