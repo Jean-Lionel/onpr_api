e09eeeb34230f95b3e6e1e610635eaae8f2efa13
Jean-Lionel <nijeanlionel@gmail.com>
2022-07-20 10:38:21 +0200

    some default midlewire

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
-header('Access-Control-Allow-Origin: *');  
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS, post, get');
+header("Access-Control-Max-Age", "3600");
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+header("Access-Control-Allow-Credentials", "true");
+
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
