d74334e9fd52231124af5b7e00700b3327b32417
Jean-Lionel <nijeanlionel@gmail.com>
2022-07-22 06:51:41 +0200

    cors header problem

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,8 @@
 <?php
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
+header('Access-Control-Allow-Origin: *');
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoleController;
@@ -29,48 +33,51 @@ use App\Http\Controllers\OnlineDeclarationDetacheController;
 |
 */
 
-Route::post('/register', [AuthController::class, 'register'])->middleware('cors');
-Route::post('/login', [AuthController::class, 'login'])->middleware('cors');
 
+
+
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 Route::apiResource('youtube_medias', YoutubeMediaController::class,[
             'except' => ['store', 'destroy']
-        ])->middleware('cors');
+        ]);
 
  Route::apiResource('articles', ArticleController::class,[
             'accept' => ['index', 'show']
-        ])->middleware('cors');
+        ]);
 
 Route::apiResource('youtube_medias', ArticleController::class,[
         'except' => ['index', 'show']
-    ])->middleware('cors');
+    ]);
 Route::apiResource('slides', SlideController::class, [
         'accept' => ['index', 'show']
-    ])->middleware('cors');
-Route::apiResource('file_declarations', FileDeclarationController::class)->middleware('cors');
-Route::get("/searchArticle/{key_word ?}", [ArticleController::class, 'search'])->middleware('cors');
-Route::get("/toutArticles", [ArticleController::class, 'toutArticles'])->middleware('cors');
+    ]);
+Route::apiResource('file_declarations', FileDeclarationController::class);
+Route::get("searchArticle/{key_word ?}", [ArticleController::class, 'search']);
+Route::get("toutArticles", [ArticleController::class, 'toutArticles']);
 
-Route::get("cotisations_afiliers/{matricule}", [CotisationAfilierController::class, "searchByMatricule"])->middleware('cors');
+Route::get("cotisations_afiliers/{matricule}", [CotisationAfilierController::class, "searchByMatricule"]);
 
-Route::get("cotisation_detaches/{matricule}", [CotisationDetacheController::class, "searchByMatricule"])->middleware('cors');
+Route::get("cotisation_detaches/{matricule}", [CotisationDetacheController::class, "searchByMatricule"]);
 
-Route::post("declaration", [DeclarationController::class, 'store'])->middleware('cors');
+Route::post("declaration", [DeclarationController::class, 'store']);
 
  Route::apiResource('adminheades', AdminHeaderController::class,[
             'accept' => ['index', 'show']
-        ])->middleware('cors');
+        ]);
     Route::apiResource('admin_contents', AdminContentController::class,[
             'accept' => ['index', 'show']
-        ])->middleware('cors');
+        ]);
      Route::apiResource('downloawddoc', DownloawdDocController::class,[
             'accept' => ['index', 'show']
-        ])->middleware('cors');
+        ]);
      Route::apiResource('annonces', AnnonceController::class,[
             'accept' => ['index', 'show']
-        ])->middleware('cors');
-    Route::middleware(['auth:sanctum','cors'])->group(function () {
+        ]);
+    Route::middleware(['auth:sanctum'])->group(function () {
     
-    Route::get('/me',[AuthController::class, 'me']);
+    Route::get('me',[AuthController::class, 'me']);
 
     Route::get('list_chargements', [ CotisationAfilierController::class, 'list_chargements']);
     // Route::post('/articles', [ArticleController::class, 'store']);
@@ -86,9 +93,7 @@ Route::post("declaration", [DeclarationController::class, 'store'])->middleware(
     Route::apiResource('annonces', AnnonceController::class,[
             'except' => ['index', 'show']
         ]);
-
     // Route::get("users/institution/{id}", [UserController::class, 'get_user_by_instutions' ])
-
     Route::get('get_user_by_instutions/{institution_id}', [InstitutionController::class, 'get_user_by_instutions'
     ]);
     Route::get('get_user_instution', [InstitutionController::class, 'getUserInstution'
@@ -104,14 +109,10 @@ Route::post("declaration", [DeclarationController::class, 'store'])->middleware(
         ]);
     Route::apiResource('declarations', DeclarationController::class);
     Route::get('declarations/search/{search_key}', [DeclarationController::class, 'search']); 
-
-    Route::post('/cotisations', [CotisationAfilierController::class, 'saveUploadData']);
-    Route::post('/cotisations_detaches', [CotisationDetacheController::class, 'saveUploadData']);
-
+    Route::post('cotisations', [CotisationAfilierController::class, 'saveUploadData']);
+    Route::post('cotisations_detaches', [CotisationDetacheController::class, 'saveUploadData']);
     Route::get("unReadDeclaration", [DeclarationController::class, 'unReadDeclaration']);
-
     Route::apiResource('users', UserController::class);
-
     Route::apiResource('cotisation_detaches', CotisationDetacheController::class);
     Route::apiResource('online_declaration_detaches', OnlineDeclarationDetacheController::class);
     Route::apiResource('cotisation_afiliers', CotisationAfilierController::class);
@@ -121,9 +122,23 @@ Route::post("declaration", [DeclarationController::class, 'store'])->middleware(
     Route::get('institutions/search/{search_key}', [InstitutionController::class, 'search']);  
     Route::get('users/search/{search_key}', [UserController::class, 'search']);
     Route::apiResource('roles', RoleController::class);
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('/clear', function (Request $request) {
+    
+    $clearconfig = Artisan::call('config:cache');
+    echo "Config cleared<br>";
+    
+    $clearcache = Artisan::call('cache:clear');
+    echo "Cache cleared<br>";
+    $clearview = Artisan::call('view:clear');
+    echo "View cleared<br>";
+
+    echo "MIGRATE SUCCES";
 });
